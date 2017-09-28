4e8b46ed10d0097cc3cca6723c3665734661e955
Shane DeWael <dewaels@purdue.edu>
2017-09-28 17:02:17 -0400

    Add login view

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react';
 import ProgressBar from './ProgressBar';
 import { connect } from 'react-redux';
 import { loginFromJWT } from '../actions/users';
+import '../assets/register.css';
+import logo from '../assets/botbot-logo.png';
 
 export class Login extends Component {
 	constructor (props) {
         super(props);
         this.state = { redirectToReferrer: false };
     }
-    
+
 	render () {
 		return (
-			<div>
-			  <ProgressBar progress='80' />
-			</div>
+			<div style={{height: '100%'}}>
+			    <ProgressBar progress='25' />
+
+			    <div id="login">
+				    <div className="row">
+				    	<div className="logo">
+				    		<img src={logo} alt="logo"/>
+				    	</div>
+				        <div className="login-form">
+				        	<form>
+							  <input className="email-input" type="text" name="email" placeholder="Email" /><br/>
+							  <input className="password-input" type="text" name="password" placeholder="Password" /><br/>
+							  <input className="submit-button" type="submit" value="Login" />
+							</form>
+				        </div>
+				    </div>
+				</div>
+
+			  </div>
 		);
 	}
 }
