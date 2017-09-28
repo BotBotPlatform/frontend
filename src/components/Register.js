e64aee9a6e8fd5ce48fc9f073bf3e9a333df740e
Shane DeWael <dewaels@purdue.edu>
2017-09-28 15:17:37 -0400

    Creates registration view

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ProgressBar from './ProgressBar';
+import '../assets/register.css';
+import logo from '../assets/botbot-logo.png'
 
 const Register = () => (
-  <div>
+  <div style={{height: '100%'}}>
     <ProgressBar progress='25' />
 
-    <img src={require('../assets/botbot-logo.png')} width="30%"/>
+    <div id="registration">
+	    <div className="row">
+	    	<div className="logo">
+	    		<img src={logo} alt="logo"/>
+	    	</div>
+	        <div className="registration-form">
+	        	<form>
+				  <input className="email-input" type="text" name="email" placeholder="Email" /><br/>
+				  <input className="password-input" type="text" name="password" placeholder="Password" /><br/>
+				  <input className="submit-button" type="submit" value="Register Now" />
+				</form>
+	        </div>
+	    </div>
+	</div>
+
   </div>
 )
 
