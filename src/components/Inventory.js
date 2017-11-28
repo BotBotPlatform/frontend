962609a22d12c79b86c9792b21188cdf359310ef
Shane DeWael <dewaels@purdue.edu>
2017-11-28 17:38:36 -0500

    Adds inventory URL

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -38,14 +38,35 @@ export class Inventory extends Component {
         });
     }
 
+    handleInventoryURL = (data) => {
+    	data.preventDefault();
+    	let form = new FormData();
+    	form.append('shop', data.target.shop.value);
+    	return apiService('shop/set', {
+    		method: 'POST',
+    		body: form
+    	}).then((res) => res.json())
+    		.then((json) => {
+    			if (json.message == 'success') {
+    				console.log('WOOHOO!');
+    			}
+    		})
+    }
+
 	render () {
 		return (
 			<div style={{height: '100%'}}>
 			    <ProgressBar progress='100' />
-                <div id="dashboard">
-                    
-                </div>
 
+                <div className="login-form">
+		        	<form onSubmit={this.handleInventoryURL}>
+					  http://etsy.com/shop/<input className="url-input" type="text" name="shop" placeholder="Etsy Shop Name" /><br/>
+					  <input className="submit-button" type="submit" value="Save URL" />
+					</form>
+		       	</div>
+
+                <button onClick={() => this.toggleFeedback(0)}>Disable Feedback</button><br/>
+                <a href="./dashboard">Dashboard</a><br/>
 			</div>
 		);
 	}
